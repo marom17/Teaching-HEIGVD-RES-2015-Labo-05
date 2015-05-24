d902857a58293035e5fd70a769f3f7559130a51a
marom17 <romain.flac@gmail.com>
2015-05-24 22:41:58 +0200

    Loadbalancer transféré sur une debian + script de démarrage lb

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Démarage du loadbalancer
-docker rm loadbalancer
-docker run -d -p 80:80 --name loadbalancer rom/lb httpd-foreground
+docker start dockerui
+docker start loadbalancer
 
--- startui.sh
+++ startui.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 9000:9000 --name dockerui --privileged -v /var/run/docker.sock:/var/run/docker.sock rom/ui
+docker start dockerui
